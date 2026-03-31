81d86b265789b0c147a1943e56d9084ab187797e
ojedarcelo <marcelo.ojeda@uc.cl>
2026-03-31 20:07:19 -0300

    support for all geometries

--- lyafit/lya_model.py
+++ lyafit/lya_model.py
@@ -2,6 +2,9 @@ import numpy as np
 import Lya_zelda_II as Lya
 from lyafit.aux_funcs import generate_igm_transmission, build_full_theta
 
+# GLOBAL CACHE: Prevents multiprocessing from pickling the grid 
+# and corrupting the memory-mapped arrays across CPU cores.
+_GRID_CACHE = {}
 
 class LyaModel:
     """
@@ -30,6 +33,23 @@ class LyaModel:
         GRIDS_LOCATION = self.ConfigFile['GridsFolder']
         Lya.funcs.Data_location = GRIDS_LOCATION
 
+    def _get_grid(self):
+        """
+        Fetches the grid from the global cache. If it doesn't exist in the 
+        current worker's memory, it loads it.
+        """
+        global _GRID_CACHE
+        cache_key = (self.model_type, self.mode)
+        
+        if cache_key not in _GRID_CACHE:
+            try:
+                _GRID_CACHE[cache_key] = Lya.load_Grid_Line(self.model_type, MODE=self.mode)
+            except TypeError:
+                # Fallback if an older version of zELDA doesn't accept MODE for this geometry
+                _GRID_CACHE[cache_key] = Lya.load_Grid_Line(self.model_type)
+                
+        return _GRID_CACHE[cache_key]
+
     def lnprior(self, theta):
         for i, pname in enumerate(self.free_params):
             bounds = self.ConfigFile[pname + 'Bounds']
@@ -38,11 +58,10 @@ class LyaModel:
         return 0.0
 
     def lnlike(self, theta, measured_wavelength, measured_flux, sigma):
-
-        # Build full parameter dictionary
         p = build_full_theta(self.param_names, self.ConfigFile, theta)
-
-        LyaRT_Grid = Lya.load_Grid_Line(self.model_type, MODE=self.mode)
+        
+        # Load grid cleanly from the worker's RAM
+        grid = self._get_grid()
 
         w_IGM_rest_Arr, T_IGM_Arr = generate_igm_transmission(
             measured_wavelength,
@@ -51,29 +70,32 @@ class LyaModel:
         )
 
         y_model_w_Arr, y_model_f_Arr, _, info = Lya.Generate_a_real_line(
-            z_t=p["Redshift"],           # Redshift
-            V_t=p["ExpV"],           # Expansion velocity
-            log_N_t=p["LogN"],       # Column density
-            t_t=p["Tau"],           # Optical depth
-            F_t=p["Flux"],           # Total flux of the line
-            log_EW_t=p["LogEW"],      # Log of the equivalent width
-            W_t=p["IntrinsicW"],           # Intrinsic width
-            PNR_t=self.ConfigFile['SNR'],            # snr global maximum
-            FWHM_t=self.fwhm_t,            # FWHM of the line
-            PIX_t=self.pix_t,             # pixel size in wavelength
-            DATA_LyaRT=LyaRT_Grid,  # grid
-            Geometry=self.model_type,     # outflow geometry
-            T_IGM_Arr=T_IGM_Arr,    # IGM transmission
-            w_IGM_Arr=w_IGM_rest_Arr,  # IGM wavelength array
+            z_t=p["Redshift"],           
+            V_t=p["ExpV"],           
+            log_N_t=p["LogN"],       
+            t_t=p["Tau"],           
+            F_t=p["Flux"],           
+            log_EW_t=p["LogEW"],      
+            W_t=p["IntrinsicW"],           
+            PNR_t=self.ConfigFile['SNR'],            
+            FWHM_t=self.fwhm_t,            
+            PIX_t=self.pix_t,             
+            DATA_LyaRT=grid,  
+            Geometry=self.model_type,     
+            T_IGM_Arr=T_IGM_Arr,    
+            w_IGM_Arr=w_IGM_rest_Arr,  
             RETURN_ALL=True,
         )
+
+        # SAFETY CATCH: If the walkers wander out of the grid bounds and zELDA 
+        # returns an empty or broken array, penalize the likelihood so walkers turn back.
+        if np.all(y_model_f_Arr == 0) or np.any(np.isnan(y_model_f_Arr)):
+            return -np.inf
 
         y_model_f_Arr = np.interp(
             measured_wavelength, y_model_w_Arr, y_model_f_Arr
         )
-        # sigma = sigma/np.max(measured_flux)
-        # measured_flux = measured_flux/np.max(measured_flux)
-        # y_model_f_Arr = y_model_f_Arr/np.max(y_model_f_Arr)
+        
         return -0.5 * np.sum(np.log(2 * np.pi * sigma ** 2) +
                              (measured_flux - y_model_f_Arr) ** 2 /
                              sigma ** 2)
@@ -94,12 +116,10 @@ class LyaModel:
         return lp + lnMeasured
 
     def generate_and_resample(self, w_Arr, z_t, V_t, log_N_t, t_t, F_t, log_EW_t, W_t, T_p):
-
-        Geometry = self.model_type
-        LyaRT_Grid = Lya.load_Grid_Line(Geometry, MODE='LIGHT')
-
         w_IGM_rest_Arr, T_IGM_Arr = generate_igm_transmission(
             w_Arr, T_p=T_p, z=z_t)
+
+        grid = self._get_grid()
 
         w_One_Arr_MCMC, f_One_Arr_MCMC, _, info = Lya.Generate_a_real_line(
             z_t=z_t,
@@ -112,8 +132,8 @@ class LyaModel:
             PNR_t=self.ConfigFile['SNR'],
             FWHM_t=self.fwhm_t,
             PIX_t=self.pix_t,
-            DATA_LyaRT=LyaRT_Grid,
-            Geometry=Geometry,
+            DATA_LyaRT=grid,
+            Geometry=self.model_type,
             T_IGM_Arr=T_IGM_Arr,
             w_IGM_Arr=w_IGM_rest_Arr,
             RETURN_ALL=True
